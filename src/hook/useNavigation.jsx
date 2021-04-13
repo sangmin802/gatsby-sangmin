00d82897d77ea673630a832390769aa4bf8164fd
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-04-13 15:32:27 +0900

    1. caching category, tag value with window.history.state

--- src/hook/useNavigation.jsx
+++ src/hook/useNavigation.jsx
@@ -1,17 +1,29 @@
 import { useCallback, useEffect, useState } from "react";
 
-export function useNavigation(resetKey = null) {
-  const [state, setState] = useState(0);
-
+export function useNavigation() {
+  const [navigation, setState] = useState({ category: 0, tag: 0 });
   const setNavigation = useCallback(
-    nav => {
-      setState(nav);
+    (nav, key) => {
+      const newNavigation = { ...navigation };
+      if (key === "category") {
+        newNavigation.category = nav;
+        newNavigation.tag = 0;
+      } else if (key === "tag") {
+        newNavigation.tag = nav;
+      }
+      setState(newNavigation);
+      window.history.replaceState(newNavigation, "", window.location.pathname);
     },
-    [setState]
+    [setState, navigation]
   );
 
   useEffect(() => {
-    return () => (resetKey ? setState(0) : null);
-  }, [resetKey, setState]);
-  return { state, setNavigation };
+    const history = window.history.state;
+    if (!history.key) {
+      setState({ ...history });
+    }
+    return;
+  }, [setState]);
+
+  return { navigation, setNavigation };
 }
