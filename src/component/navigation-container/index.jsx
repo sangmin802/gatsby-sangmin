6442db0559400821982688dbe73b1296d4cd4841
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-04-15 15:01:01 +0900

    slide navigation 수정 transform이 아닌, scroll api 활용 - 적영되는 값이 부모 영역 내부일때만 작동되는점이 큰 매력인듯  즉, -값이나 부모보다 큰 값은 적용 안됨

--- src/component/navigation-container/index.jsx
+++ src/component/navigation-container/index.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import Navigation from "../navigation/index";
 import "./index.scss";
 
 const NavigationContainer = ({ nameArr, setNavigation, selected, type }) => {
+  const container = useRef(null);
+  const scrollContainer = useCallback(
+    target => {
+      const parent = container.current;
+      const targetStart = target.getBoundingClientRect().x;
+      const parentStart = parent.getBoundingClientRect().x;
+      const prevScrolled = parent.scrollLeft;
+      const targetWidth = target.clientWidth;
+      const targetX = targetStart - parentStart;
+
+      // Operates only when the change is in the parent internal area
+      parent.scroll({
+        left: targetX + prevScrolled - parent.clientWidth / 2 + targetWidth / 2,
+        behavior: "smooth",
+      });
+    },
+    [container]
+  );
   return (
-    <article className="navigation">
-      <div className="inner-navigation">
-        {nameArr.map((name, index) => (
-          <Navigation
-            key={name}
-            name={name}
-            index={index}
-            type={type}
-            setNavigation={setNavigation}
-            selected={selected}
-          />
-        ))}
-      </div>
+    <article className="navigation" ref={container}>
+      {nameArr.map((name, index) => (
+        <Navigation
+          key={name}
+          name={name}
+          index={index}
+          type={type}
+          setNavigation={setNavigation}
+          selected={selected}
+          scrollContainer={scrollContainer}
+        />
+      ))}
     </article>
   );
 };
--- src/component/navigation/index.jsx
+++ src/component/navigation/index.jsx
@@ -1,42 +1,34 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 
-const Navigation = ({ name, index, setNavigation, selected, type }) => {
-  const selectNavigation = useCallback(
-    e => {
-      setNavigation(index, type);
+const Navigation = ({
+  name,
+  index,
+  setNavigation,
+  selected,
+  type,
+  scrollContainer,
+}) => {
+  const target = useRef(null);
 
-      const visibleWidth = document.querySelector(".fixed-navigation")
-        .clientWidth;
-      const parentNode = e.target.parentNode;
-      const parentWidth = parentNode.clientWidth;
-      const half = visibleWidth / 2;
-      const parentX = parentNode.getBoundingClientRect().x;
-      const targetX = e.target.getBoundingClientRect().x;
-      const relativeX = targetX - parentX;
-      let gap = 0;
-      let targetWidth = 0;
-      if (relativeX > half) {
-        gap = relativeX - half;
-        targetWidth = e.target.getBoundingClientRect().width / 2;
-      }
-      if (relativeX >= parentWidth / 2) {
-        targetWidth = 0;
-        gap = parentWidth - visibleWidth;
-      }
-      const style = `
-        transform : translate(${-(gap + targetWidth)}px)
-      `;
-      parentNode.setAttribute("style", style);
-    },
-    [index, setNavigation, type]
-  );
+  const scrollCenter = useCallback(() => {
+    const { current } = target;
+    scrollContainer(current);
+  }, [scrollContainer, target]);
+
+  const selectNavigation = useCallback(() => {
+    setNavigation(index, type);
+  }, [index, setNavigation, type]);
 
   const style = useMemo(() => {
     return index === selected ? { color: "#000" } : {};
   }, [index, selected]);
 
+  useEffect(() => {
+    if (selected === index) scrollCenter();
+  }, [scrollCenter, selected, index]);
+
   return (
-    <nav style={style} onClick={selectNavigation}>
+    <nav style={style} onClick={selectNavigation} ref={target}>
       {name}
     </nav>
   );
